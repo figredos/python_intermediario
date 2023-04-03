781f39219a8468bd8fa53330c4844dda2dc4dd45
figredos <matheusmfellet@gmail.com>
2023-04-03 11:22:47 -0300

    Introdução a sets em python.

---
+++ sets/sets.py
@@ -0,0 +1,10 @@
+set_1 = set()  # Sets vazios
+
+set_2 = {'Matheus', 1, 2, 3}  # Sets com dados
+
+set_3 = {1, 1, 1, 1, 1, 1, 3, 3, 3, 4}  # Eliminam dados repetidos automaticamente que são passados para ele
+
+lista_1 = [1, 2, 3, 5, 4, 6, 46, 1, 2, 3, 3, 5, 5, 46, 46, 42]
+set_4 = set(lista_1)  # Eliminam dados repetidos de outras estruturas de dados, como listas.
+
+#  Contudo, sets não garantem ordem.
