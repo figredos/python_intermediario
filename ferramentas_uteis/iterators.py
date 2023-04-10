dbe9c8779ea09b392aecf18697bac7611cd83ca4
figredos <matheusmfellet@gmail.com>
2023-04-10 14:05:37 -0300

    Um breve resumo de iteraveis e iterators em python.

---
+++ ferramentas_uteis/iterators.py
@@ -0,0 +1,8 @@
+# Iterables e Iterators
+iterable = ['Eu', 'Tenho', '__iter__']
+iterator = iter(iterable)   # Tem __iter__ e __next__
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
