7752783c7419fcc013880bc621154c75e2912a3e
figredos <matheusmfellet@gmail.com>
2023-04-14 15:48:27 -0300

    Raise, levantando erros no python.

---
+++ tratamento_de_erros/raise.py
@@ -0,0 +1,21 @@
+# Função que gera um erro ao tentar dividir por zero
+def erro_divide_por_zero(numero):
+    if numero == 0:
+        raise ZeroDivisionError('Você está tentando dividir por zero!')
+    return True
+
+
+# Função que gera um erro se o parâmetro numero for diferente de int ou float
+def deve_ser_int_ou_float(numero):
+    if not isinstance(numero, (float, int)):
+        raise TypeError(
+            f'{numero} deve ser int ou float.'
+        )
+
+
+# Função que checa os dados com tratamento de exceções e retorna a divisão entre os parâmetros
+def divide(numerador, divisor):
+    deve_ser_int_ou_float(numerador)
+    deve_ser_int_ou_float(divisor)
+    erro_divide_por_zero(divisor)
+    return numerador / divisor
