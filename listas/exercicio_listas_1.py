46a5b9d54edcd1bdac4898332c968f66fc496e79
figredos <matheusmfellet@gmail.com>
2023-04-25 12:06:13 -0300

    Exercicios com listas em python.

---
+++ listas/exercicio_listas_1.py
@@ -0,0 +1,28 @@
+# Exercício - Unir listas
+# Crie uma função zipper (como o zipper de roupas)
+# O trabalho dessa função será unir duas
+# listas na ordem.
+# Use todos os valores da menor lista.
+# Ex.:
+# ['Salvador', 'Ubatuba', 'Belo Horizonte']
+# ['BA', 'SP', 'MG', 'RJ']
+# Resultado
+# [('Salvador', 'BA'), ('Ubatuba', 'SP'), ('Belo Horizonte', 'MG')]
+
+def zipper(list_1, list_2):
+    is_list(list_1, list_2)
+    new_list = [(list_1[i], list_2[i]) for i in range(min(len(list_1), len(list_2)))]
+    return new_list
+
+
+def is_list(*args):
+    for arg in args:
+        if not isinstance(arg, list):
+            raise TypeError('Parâmetro deve ser do tipo list.')
+
+
+lista_1 = ['Salvador', 'Ubatuba', 'Belo Horizonte']
+lista_2 = ['BA', 'SP', 'MG', 'RJ']
+nova_lista = zipper(lista_1, lista_2)
+
+print(nova_lista)
