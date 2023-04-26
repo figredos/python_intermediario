3d6463027c3059df40b4af99455f8152e03dea60
figredos <matheusmfellet@gmail.com>
2023-04-25 21:00:44 -0300

    Usando a função zip em python.

---
+++ itertools/zip.py
@@ -0,0 +1,8 @@
+# Criando as listas
+lista_1 = ['Salvador', 'Ubatuba', 'Belo Horizonte']
+lista_2 = ['BA', 'SP', 'MG', 'RJ']
+
+# Agregando os elementos de uma lista com a outra
+lista_agregada = list(zip(lista_1, lista_2))
+
+print(lista_agregada)
