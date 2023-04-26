cefa9cf31c3fdf157f38a22c06867f4a850e0fe6
figredos <matheusmfellet@gmail.com>
2023-04-25 21:07:29 -0300

    Usando a função zip em python.

--- itertools/zip.py
+++ itertools/zip.py
@@ -1,8 +1,12 @@
+from itertools import zip_longest
+
 # Criando as listas
 lista_1 = ['Salvador', 'Ubatuba', 'Belo Horizonte']
 lista_2 = ['BA', 'SP', 'MG', 'RJ']
 
 # Agregando os elementos de uma lista com a outra
 lista_agregada = list(zip(lista_1, lista_2))
+lista_agregada_reversa = list(zip_longest(lista_1, lista_2))
 
 print(lista_agregada)
+print(lista_agregada_reversa)
